55d93fa2d2597b91388c5e0a4556edc6dddea70d
Bikita Suwal <139045699+bikitasuwal@users.noreply.github.com>
2025-09-23 20:55:10 +0545

    updated the Route to be in main.jsx instead of App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,34 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />}>
+        <Route  path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/activities' element={<Activities />} />
+      <Route path='/contactus' element={<Contactus />} />
+      <Route path='/donate' element={<Donate />} />
+      <Route path='/members' element={<Members />} />
+      <Route path='/messages' element={<Messages />} />
+      <Route path='/objectives' element={<Objectives />} />
+      <Route path='/ourteam' element={<Ourteam />} />
+      <Route path='/partners' element={<Partners />} />
+      <Route path='/projectpartnership' element={<ProjectPartnership />} />
+      <Route path='/photogallery' element={<PhotoGallery />} />
+      <Route path='/gallery' element={<Gallery />} />
+      <Route path='/projects' element={<Projects />} />
+      <Route path='/staffs' element={<Staffs />} />
+      <Route path='/supportus' element={<Supportus />} />
+      <Route path='/donations' element={<Donations />} />
+      <Route path='/pastactivities' element={<PastActivities />} />
+      <Route path='/upcomingactivities' element={<UpcomingActivities />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
 )
